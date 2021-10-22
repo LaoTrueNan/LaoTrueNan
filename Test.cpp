20225e0dad7432eda934869e26b66bb0708fde54
LaoTrueNan <leonard.qiang@gmail.com>
2021-10-22 16:40:38 +0800

    iii

--- Test.cpp
+++ Test.cpp
@@ -2,6 +2,6 @@
 int main(){
     Term t(5,2);
     t.insertAfter(4,1);
-    t.insertAfter(5,2);
-    cout<<t<<endl;
+    t.insertAfter(1,3);
+    cout<t.link<<endl;
 }
--- readme.md
+++ readme.md
@@ -2,3 +2,7 @@ java 1.8
 jre is not neededa
 jdk 11
 this is all me
+#### jaf
+```java
+#include
+
